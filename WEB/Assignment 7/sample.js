e7d50be81efaa2dae078b6f9be135698296807d1
Mayuresh Powar <156998876+mayureshpowar4889@users.noreply.github.com>
2025-09-27 18:59:39 +0530

    Add files via upload

---
+++ WEB/Assignment 7/sample.js
@@ -0,0 +1,9 @@
+const person = {};
+
+person.firstName = "Jhon";
+person.lastname = "Doe";
+person.age = 22;
+
+let text = person;
+console.log(text);
+console.log(8*"a");
---
+++ WEB/Assignment 8/sort.js
@@ -0,0 +1,8 @@
+
+let num =[88, 2, 41, 1];
+num.sort((n1,n2) =>n1-n2);
+console.log(num);
+let arr = ['banana','apple','cherry'];
+arr.sort();
+console.log(arr)
+
